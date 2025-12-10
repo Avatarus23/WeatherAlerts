a6fc7cec9c20ece960e7f772c0ad9b8cad56f653
Avatarus23 <bobi.galic@yahoo.com>
2025-12-10 12:08:15 +0100

    init

---
+++ weather-producer-service/src/main/java/mk/ukim/finki/weatherproducerservice/WeatherProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.weatherproducerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProducerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherProducerServiceApplication.class, args);
+    }
+
+}
